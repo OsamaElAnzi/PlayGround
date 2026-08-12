73aabe499ce0ae693b743e196df3509bc16c787d
Osama El Anzi <osamaelanzi0@gmail.com>
2026-08-12 11:28:16 +0200

    Created a likely identical Iphone calculator style

--- Usefull/src/view/CalculatorView.tsx
+++ Usefull/src/view/CalculatorView.tsx
@@ -8,17 +8,20 @@ export default function CalculatorView() {
     const isDarkMode = useColorScheme() === 'dark';
 
     const rows = [
-        ['del', 'C', '%', '/'],
+        ['<', 'AC', '%', '/'],
         ['7', '8', '9', 'x'],
-        ['4', '5', '6', 'x'],
-        ['1', '2', '3', 'x'],
-        ['x', '0', ',', '='],
+        ['4', '5', '6', '+'],
+        ['+/-', '0', ',', '='],
     ];
+    const specialButtonsRight = ['/', 'x', '+', '='];
+    const specialButtonsOthers = ['<', 'AC', '%'];
+
+    const handleButtonPress = (button: string) => {
     return (
         <View style={{flex: 1, backgroundColor: isDarkMode ? '#226666' : '#93D1D1', padding: 20}}>
             <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
                 <View style={styles.CategoryContainer}>
-                    <Text style={styles.CalculatorInput} >{value}</Text>
+                    <Text style={styles.CalculatorInput} >{value.toString().split('').reverse().join('')}</Text>
                     {rows.map((row, rowIndex) => (
                         <View key={rowIndex} style={{flexDirection: 'row', justifyContent: 'space-between'}}>
                             {row.map((button, buttonIndex) => (
@@ -26,7 +29,7 @@ export default function CalculatorView() {
                                     key={buttonIndex}
                                     onPress={() => console.log(`Button ${button} pressed`)}
                                 >
-                                    <Text style={styles.Button}>{button}</Text>
+                                    <Text style={specialButtonsRight.includes(button) ? styles.SpecialButtonRight : specialButtonsOthers.includes(button) ? styles.SpecialButtonOthers : styles.Button}>{button}</Text>
                                 </TouchableOpacity>
                             ))}
                         </View>
@@ -57,11 +60,47 @@ const styles = StyleSheet.create({
         padding: 10,
         fontSize: 54,
         color: '#ffffff',
+        paddingBottom: 150,
+        top: 140,
+        left: 250,
     },
     Button: {
-        padding: 10,
-        fontSize: 54,
+        borderRadius: 50,
+        fontSize: 34,
+        backgroundColor: '#9cacac',
+        margin: 5,
         color: '#ffffff',
-        top: 80,
+        width: 65,
+        height: 65,
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        paddingTop: 13,
     },
+    SpecialButtonRight: {
+        borderRadius: 50,
+        fontSize: 34,
+        backgroundColor: '#ff9900',
+        margin: 5,
+        color: '#ffffff',
+        width: 65,
+        height: 65,
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        paddingTop: 13,
+    },
+    SpecialButtonOthers: {
+        borderRadius: 50,
+        fontSize: 34,
+        backgroundColor: '#6f7373',
+        margin: 5,
+        color: '#ffffff',
+        width: 65,
+        height: 65,
+        justifyContent: 'center',
+        alignItems: 'center',
+        textAlign: 'center',
+        paddingTop: 13,
+    }
 }); 
