27ae7c1d768f3167bd4844926dbe5243a120acfe
Osama El Anzi <osamaelanzi0@gmail.com>
2026-08-15 16:08:47 +0200

    designed a kind of clock, but still looks shitty

--- Usefull/src/view/ClockView.tsx
+++ Usefull/src/view/ClockView.tsx
@@ -1,27 +1,32 @@
 import {View, Text, StyleSheet, SafeAreaView, TouchableOpacity, useColorScheme} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import { useState, useEffect } from 'react';
 
 export default function ClockView() {
     const isDarkMode = useColorScheme() === 'dark';
+        
+    const [time, setTime] = useState(new Date());
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTime(new Date());
+        }, 1000);
+
+        return () => clearInterval(timer);
+    }, []);
     return (
-        <View style={{flex: 1, backgroundColor: isDarkMode ? '#000000' : '#ffffff', padding: 20}}>
-            <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-                
-            </View>
+        <View style={{flex: 1,backgroundColor: isDarkMode ? '#000000' : '#ffffff'}}>
+            <Text style={styles.sectionTitle}>{time.toLocaleTimeString()}</Text>
         </View>
-    );  
+    );
 }
 
 const styles = StyleSheet.create({
     sectionTitle: {
-        fontSize: 24,
-        fontWeight: '600',
-        color: '#ffffff',
-        flex: 1
-    },
-    CategoryContainer: {
-        backgroundColor: '#000000',
-        borderRadius: 10,
+        top: 150,
+        fontSize: 120,
+        color: '#000000',
+        transform: [{rotate: '90deg'}],
     },
 });
